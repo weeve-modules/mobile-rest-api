0bba8268a2d0f11b4497d6aff5874f8687bb2653
mesud pasic <839950+mesudpasic@users.noreply.github.com>
2022-05-13 10:31:56 +0200

    updating rest service for mobile app with some minor improvements and support for scheduler endpoint

--- app.js
+++ app.js
@@ -1,334 +1,135 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const { MongoClient } = require("mongodb");
-var ObjectId = require("mongodb").ObjectID;
+const { formatTimeDiff } = require('./utils/util')
+const ObjectId = require('mongodb').ObjectID
+const {
+  getUserPasswordByName,
+  getLocationByLocationID,
+  getDeviceByDeviceID,
+  updateRoomTemperature,
+  toggleOnOffRoom,
+  updateRoomName,
+  updateRoomPlan,
+  getDevicesList,
+} = require('./utils/mongodb')
+const config = require('config')
+const winston = require('winston')
+const expressWinston = require('express-winston')
+const express = require('express')
 
-app.use(express.json());
+const settings = config[process.env.NODE_ENV]
 
-const uri =
-  "mongodb://wohnio:wohnio@mongodb.wohnio.weeve.engineering:27017/wohnio?authSource=wohnio";
+// initialization
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-const client = new MongoClient(uri);
-const dbname = "wohnio";
+// logger
+app.use(
+  expressWinston.logger({
+    transports: [
+      new winston.transports.Console(),
+      /*
+    new winston.transports.File({
+        filename: 'logs/scheduler.log'
+    })
+    */
+    ],
+    format: winston.format.combine(winston.format.colorize(), winston.format.json()),
+    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+    msg: 'HTTP {{req.method}} {{req.url}}', // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+    colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+    ignoreRoute: function (req, res) {
+      return false
+    }, // optional: allows to skip some log messages based on request and/or response
+  })
+)
+const startTime = Date.now()
+// health check
+app.get('/health', async (req, res) => {
+  res.json({
+    serverStatus: 'Running',
+    uptime: formatTimeDiff(Date.now(), startTime),
+  })
+})
 
-// const port = process.env.PORT || 80;
-const port = process.env.PORT || 443;
+app.post('/devices', async (req, res) => {
+  const json = req.body
+  const devices = await getDevicesList(json.location)
+  res.send({
+    data: devices,
+  })
+})
 
-app.post("/checkUser", async (request, res) => {
-  connectToClient(client, uri).then(async (response) => {
-    getUserPasswordByName(
-      client,
-      dbname,
-      "user",
-      request.body.email,
-      request.body.password
-    ).then(async (response) => {
-      res.send(response);
-      // await client.close();
-    });
-  });
-});
+app.post('/checkUser', async (req, res) => {
+  getUserPasswordByName('user', req.body.email, req.body.password).then(async response => {
+    res.send(response)
+    // ;
+  })
+})
 
-app.post("/getLocationsUser", async (request, res) => {
-  connectToClient(client, uri).then(async (response) => {
-    getLocationByLocationID(
-      client,
-      dbname,
-      "location",
-      request.body.locationID
-    ).then(async (response) => {
-      res.send(response);
-      // await client.close();
-    });
-  });
-});
+app.post('/getLocationsUser', async (req, res) => {
+  getLocationByLocationID('location', req.body.locationID).then(async response => {
+    res.send(response)
+    // ;
+  })
+})
 
-app.post("/getDevicesUser", async (request, res) => {
-  connectToClient(client, uri).then(async (clientRes) => {
-    getDeviceByDeviceID(client, dbname, "device", request.body.deviceID).then(
-      async (response) => {
-        res.send(response);
-        await client.close();
-      }
-    );
-  });
-});
+app.post('/getDevicesUser', async (req, res) => {
+  getDeviceByDeviceID('device', req.body.deviceID).then(async response => {
+    res.send(response)
+  })
+})
 
-app.post("/updateRoomTemperature", async (request, res) => {
-  console.log("___ update room temperature ___");
-  connectToClient(client, uri).then(async (response) => {
-    updateRoomTemperature(
-      client,
-      dbname,
-      "device",
-      ObjectId(request.body.idSensor),
-      {
-        manualTemperature: {
-          command: {
-            name: "setTemperatur",
-            params: {
-              value: request.body.temperature,
-              until: "1970-01-01T00:00:00.000Z",
-            },
-          },
+app.post('/updateRoomTemperature', async (req, res) => {
+  updateRoomTemperature('device', ObjectId(req.body.idSensor), {
+    manualTemperature: {
+      command: {
+        name: 'setTemperatur',
+        params: {
+          value: req.body.temperature,
+          until: '1970-01-01T00:00:00.000Z',
         },
-      }
-    ).then(async (response) => {
-      res.send(response);
-      await client.close();
-    });
-  });
-});
+      },
+    },
+  }).then(async response => {
+    res.send(response)
+  })
+})
 
-app.post("/toggleOnOffRoom", async (request, res) => {
-  connectToClient(client, uri).then(async (response) => {
-    toggleOnOffRoom(client, dbname, "device", ObjectId(request.body.idSensor), {
-      isOn: request.body.isOn,
-    }).then(async (response) => {
-      res.send(response);
-      await client.close();
-    });
-  });
-});
+app.post('/toggleOnOffRoom', async (req, res) => {
+  toggleOnOffRoom('device', ObjectId(req.body.idSensor), {
+    isOn: req.body.isOn,
+  }).then(async response => {
+    res.send(response)
+  })
+})
 
-app.post("/updateRoomName", async (request, res) => {
-  connectToClient(client, uri).then(async (response) => {
-    updateRoomName(client, dbname, "device", ObjectId(request.body.idSensor), {
-      label: request.body.nameRoom,
-    }).then(async (response2) => {
-      await client.close();
-    });
-  });
-});
+app.post('/updateRoomName', async (req, res) => {
+  updateRoomName('device', ObjectId(req.body.idSensor), {
+    label: req.body.nameRoom,
+  }).then(async response2 => {})
+})
 
-app.post("/updateRoomPlan", async (request, res) => {
-  connectToClient(client, uri).then(async (response) => {
-    updateRoomPlan(client, dbname, "device", ObjectId(request.body.idSensor), {
-      schedule: request.body.scheduleSensor,
-    }).then(async (response2) => {
-      await client.close();
-    });
-  });
-});
+app.post('/updateRoomPlan', async (req, res) => {
+  updateRoomPlan('device', ObjectId(req.body.idSensor), {
+    schedule: req.body.scheduleSensor,
+  }).then(async response2 => {})
+})
 
-app.listen(port, () => {
-  console.clear();
-  console.log("******** Server running ********");
-});
-
-// Connect to Database
-async function connectToClient(client, uri) {
-  try {
-    await client.connect();
-  } catch (e) {
-    console.log("Error - " + e);
-  } finally {
-    // await client.close();
+// handle exceptions
+app.use(async (err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err)
   }
-}
+  const errCode = err.status || 401
+  res.status(errCode).send({
+    status: false,
+    message: err.message,
+  })
+})
 
-// Get Username and Password
-async function getUserPasswordByName(
-  client,
-  dbname,
-  collectionname,
-  _email,
-  _password
-) {
-  try {
-    const database = await client.db(dbname);
-    const users = await database.collection(collectionname);
-
-    const cursor = users.find({ email: _email });
-    if ((await cursor.count()) === 0) {
-      return { nofound: true };
-    }
-
-    var currentUser;
-
-    await cursor.forEach((user) => {
-      currentUser = user;
-    });
-
-    if (currentUser.password != _password) {
-      return { falsepassword: true };
-    } else {
-      return currentUser;
-    }
-  } finally {
-    // await client.close();
-  }
-}
-
-// Get Location by LocationID
-async function getLocationByLocationID(
-  client,
-  dbname,
-  collectionname,
-  _locationID
-) {
-  try {
-    const database = await client.db(dbname);
-    const locations = await database.collection(collectionname);
-
-    const newobj = {
-      _id: ObjectId(_locationID),
-    };
-    const collection = locations.find(newobj);
-    if ((await collection.count()) === 0) {
-      return { nofound: true };
-    }
-
-    var currentLocation;
-
-    await collection.forEach((location) => {
-      currentLocation = location;
-    });
-    return currentLocation;
-  } finally {
-    // await client.close();
-  }
-}
-
-// Get Sensor by SensorID
-async function getDeviceByDeviceID(client, dbname, collectionname, _sensorID) {
-  try {
-    const database = await client.db(dbname);
-    const sensors = await database.collection(collectionname);
-
-    const newobj = {
-      _id: ObjectId(_sensorID),
-    };
-
-    const collection = sensors.findOne(newobj);
-
-    var currentSensor;
-
-    // if ((await collection.count()) === 0) {
-    // return { nofound: true };
-    // } else {
-    currentSensor = collection;
-    // console.log(currentSensor);
-    // console.log(currentSensor);
-    return currentSensor;
-    // }
-
-    // var currentSensor = [];
-
-    // await collection
-    //   .forEach((sensor) => {
-    //     if (sensor["_id"].toString() === newobj._id.toString()) {
-    //       currentSensor = sensor;
-    //       console.log(currentSensor);
-    //     } else {
-    //       return { nofound: true };
-    //     }
-    //   })
-    //   .then((res) => {
-    //     console.log(res);
-    //     console.log(currentSensor);
-    //     return currentSensor;
-    //   });
-  } finally {
-    // await client.close();
-  }
-}
-
-// Update Room Temperature
-async function updateRoomTemperature(
-  client,
-  dbname,
-  collectioname,
-  nameOfListing,
-  _newData
-) {
-  try {
-    const result = await client
-      .db(dbname)
-      .collection(collectioname)
-      .updateOne({ _id: nameOfListing }, { $set: _newData });
-
-    console.log(
-      `${result.matchedCount} document(s) matched the query criteria.`
-    );
-    console.log(`${result.modifiedCount} document(s) was/were updated.`);
-
-    return { update: true };
-  } finally {
-    // await client.close();
-  }
-}
-
-// Toggle Room - On / Off
-async function toggleOnOffRoom(
-  client,
-  dbname,
-  collectioname,
-  nameOfListing,
-  _newData
-) {
-  try {
-    const result = await client
-      .db(dbname)
-      .collection(collectioname)
-      .updateOne({ _id: nameOfListing }, { $set: _newData });
-
-    console.log(
-      `${result.matchedCount} document(s) matched the query criteria.`
-    );
-    console.log(`${result.modifiedCount} document(s) was/were updated.`);
-
-    return { update: true };
-  } finally {
-    // await client.close();
-  }
-}
-
-// Update Room Name
-async function updateRoomName(
-  client,
-  dbname,
-  collectioname,
-  nameOfListing,
-  _newData
-) {
-  try {
-    const result = await client
-      .db(dbname)
-      .collection(collectioname)
-      .updateOne({ _id: nameOfListing }, { $set: _newData });
-    console.log(
-      `${result.matchedCount} document(s) matched the query criteria.`
-    );
-    console.log(`${result.modifiedCount} document(s) was/were updated.`);
-
-    return { update: true };
-  } finally {
-    // await client.close();
-  }
-}
-
-// Update Room Plan
-async function updateRoomPlan(
-  client,
-  dbname,
-  collectionname,
-  nameOfListing,
-  _newData
-) {
-  try {
-    const result = await client
-      .db(dbname)
-      .collection(collectionname)
-      .updateOne({ _id: nameOfListing }, { $set: _newData });
-    console.log(
-      `${result.matchedCount} document(s) matched the query criteria.`
-    );
-    console.log(`${result.modifiedCount} document(s) was/were updated.`);
-
-    return { update: true };
-  } finally {
-    // await client.close();
-  }
+if (require.main === module) {
+  app.listen(settings.PORT, settings.HOST, () => {
+    console.log(`Service listening on ${settings.PORT}`)
+  })
 }
